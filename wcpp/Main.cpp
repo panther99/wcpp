aefe7e6e4cf590d3192348abd5f293f9aecfa742
panther99 <nikola.stojakovic@hotmail.com>
2023-09-26 16:42:50 +0200

    Add showing total counts

--- wcpp/Main.cpp
+++ wcpp/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <format>
+#include <map>
 
 #include "args.h"
 #include "File.h"
@@ -13,6 +14,12 @@ int main(int argc, char* argv[])
 	std::string output;
 	char flags[] = { 'c', 'l', 'w', 'm' };
 	std::vector<Flag> passed_flags;
+	std::map<Flag, unsigned int> sums = {
+		{ Flag::Bytes, 0 },
+		{ Flag::Chars, 0 },
+		{ Flag::Lines, 0 },
+		{ Flag::Words, 0 }
+	};
 
 	parser.flag("c");
 	parser.flag("l");
@@ -41,23 +48,48 @@ int main(int argc, char* argv[])
 				switch (passed_flag)
 				{
 				case Flag::Bytes:
-					output.append(std::format("{}\t", f.bytes_count()));
+				{
+					auto count = f.bytes_count();
+					output.append(std::format("{}\t", count));
+					sums[Flag::Bytes] += count;
 					break;
+				}
 				case Flag::Chars:
-					output.append(std::format("{}\t", f.chars_count()));
+				{
+					auto count = f.chars_count();
+					output.append(std::format("{}\t", count));
+					sums[Flag::Chars] += count;
 					break;
+				}
 				case Flag::Lines:
-					output.append(std::format("{}\t", f.lines_count()));
+				{
+					auto count = f.lines_count();
+					output.append(std::format("{}\t", count));
+					sums[Flag::Lines] += count;
 					break;
+				}
 				case Flag::Words:
-					output.append(std::format("{}\t", f.words_count()));
+				{
+					auto count = f.words_count();
+					output.append(std::format("{}\t", count));
+					sums[Flag::Words] += count;
 					break;
+				}
 				}
 			}
 
 			output.append(std::format("{}\n", f.name()));
 		}
 	}
+
+	output.append("\t");
+
+	for (Flag passed_flag : passed_flags)
+	{
+		output.append(std::format("{}\t", sums[passed_flag]));
+	}
+
+	output.append("total");
 
 	std::cout << output << std::endl;
 
